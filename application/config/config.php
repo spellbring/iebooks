d6e619737f657f072a5dac1e6824e5c61763ee66
root <root@localhost.localdomain>
2017-04-04 10:48:57 -0700

    se cambian properties

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/iebooks';
+$config['base_url'] = 'http://192.168.183.129/iebooks';
 
 /*
 |--------------------------------------------------------------------------
